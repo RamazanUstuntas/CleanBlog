5333b7b8226ce3612334965d21b6a804565fdddb
RamazanUstuntas <ustuntasramazan@outlook.com>
2021-12-26 11:26:29 +0300

    Workspace Created

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const blog = {
+  id: 1,
+  title: 'Blog Title',
+  description: 'Blog description',
+};
+
+app.get('/', (req, res) => {
+  res.send(blog);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
