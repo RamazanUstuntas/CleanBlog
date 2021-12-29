0fac1306f971c244055a0c70be80679728245094
RamazanUstuntas <ustuntasramazan@outlook.com>
2021-12-29 17:25:37 +0300

    post update,delete and MVC models-view created

---
+++ controllers/pageController.js
@@ -0,0 +1,16 @@
+const Post = require('../models/Post');
+
+exports.getAbout = (req, res) => {
+  res.render('about');
+};
+
+exports.getEdit = async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  res.render('edit', {
+    post,
+  });
+};
+
+exports.getAddPost = (req, res) => {
+  res.render('add_post');
+};
